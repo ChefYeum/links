a5fe9335e958e3906764f206a067572ff5a83b26
chefyeum <chefyeum@gmail.com>
2021-09-06 14:57:56 +0100

    pass program to jsoutput

--- core/jsOutput.ml
+++ core/jsOutput.ml
@@ -1,7 +1,8 @@
-let code = "console.log('Hello World'))"
-
-(* Write code to file *)
-let save outputFilename = 
-  let oc = open_out outputFilename in (* create or truncate file, return channel *)
-    Printf.fprintf oc "%s\n" code; (* write something *)   
+let save outputFilename program = 
+  let _venv, code =
+    let venv = Irtojs.VEnv.empty
+    in  Irtojs.Compiler.generate_program venv program
+  in let jscode = Irtojs.Js_CodeGen.string_of_js code
+  in let oc = open_out outputFilename in (* create or truncate file, return channel *)
+    Printf.fprintf oc "%s" jscode; (* write something *)   
     close_out oc;                  (* flush and close the channel *)
