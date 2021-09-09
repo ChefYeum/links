753251121c7dca25841ed97bd75597ffe1258edf
chefyeum <chefyeum@gmail.com>
2021-09-09 09:11:14 +0100

    add jslib imports

--- core/jsOutput.ml
+++ core/jsOutput.ml
@@ -1,8 +1,18 @@
+let add_lib_import jscode = "
+var {
+  _yieldCount,
+  _yieldGranularity,
+  _yield,
+  _yieldCont_Default,
+  _yieldCont,
+} = require("./nodelib.js");
+" ^ jscode
+
 let save venv program outputFilename = 
   let _venv, code =
     (* let venv = Irtojs.VEnv.empty in *)
     Irtojs.Compiler.generate_program venv program
-  in let jscode = Irtojs.Js_CodeGen.string_of_js code
+  in let jscode = Irtojs.Js_CodeGen.string_of_js code |> add_lib_import
   in let oc = open_out outputFilename in (* create or truncate file, return channel *)
     Printf.fprintf oc "%s" jscode; (* write something *)   
     close_out oc;                  (* flush and close the channel *)
